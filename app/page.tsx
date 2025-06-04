7009833ec2c1cfb3e3256b3cd12df4918bacee37
Sogon-Njugush <sogon5045@gmail.com>
2025-06-04 22:23:41 +0300

    Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Home from "@/components/Home/Home";
 import React from "react";
 
-const Home = () => {
-  return <div>Horizon Accord Adventures page</div>;
+const Page = () => {
+  return (
+    <div className="min-h-screen">
+      <Home />
+    </div>
+  );
 };
 
-export default Home;
+export default Page;
